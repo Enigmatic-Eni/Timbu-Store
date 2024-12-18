73a197928356287eb23a06894f241b5b483db99a
Motunrayo Sanni <sannimotunrayo04@gmail.com>
2024-12-18 19:28:22 +0100

    mapped collections items

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import { IoIosHeartEmpty } from "react-icons/io";
-import bag from '../assets/bag.png'
+import { MdOutlineStarPurple500 } from "react-icons/md"
 
-export default function Collection() {
+export default function Collection({image, category, rating, brand,item,price}) {
   return (
-    <div className=' w-[392px] border border-[#C1C1C1] h-[480px] flex flex-col  rounded-2xl mr-3'>
-        <div className=' w-[342px] h-[270px] bg-[#F2F2F2] rounded-2xl m-2 '>
+    <div className=' w-[435px] border border-[#C1C1C1] h-[480px] flex flex-col  rounded-2xl mr-3 items-center mb-2'>
+        <div>
+        <div className=' w-[403px] h-[270px] bg-[#F2F2F2] rounded-2xl m-2 '>
             <div className=' p-4 flex justify-between items-center '>
                 <div className=' bg-white p-3 text-black rounded-full cursor-pointer'>
             <IoIosHeartEmpty />
             </div>
             <p className=' bg-white px-3 py-2 rounded-3xl text-sm'>In Stock</p>
             </div>
-            <div className=' items-center flex justify-center '>
-                <img src={bag}/>
+            <div className=' items-center flex justify-center max-w-sm'>
+                <img src={image}/>
             </div>
 
         </div>
@@ -22,11 +23,23 @@ export default function Collection() {
 
         <div className=' flex justify-between mx-2'>
             <div>
-                <p className=' py-1 text-sm px-3 border rounded-3xl text-[#808080]'>Women</p>
+                <p className=' py-1 text-sm px-3 border rounded-3xl text-[#808080]'>{category}</p>
             </div>
-            <div>
+            <div className=' flex items-center text-[#808080]'>
+            <MdOutlineStarPurple500 className=' text-2xl text-[#FFD700]'/>
+            <p>{rating}</p>
+            </div>
 
-            </div>
+        </div>
+<div className=' pt-5 mx-2'>
+    <p className=' text-lg font-semibold'>{item}</p>
+    <p className='  text-[#808080]'>{brand}</p>
+</div>
+
+<div className=' mx-2 flex justify-between items-center mt-3'>
+    <p className=' text-lg font-semibold'>{price}</p>
+    <button className=' bg-black text-white px-4 py-5 rounded-2xl font-medium'>Add to Cart</button>
+</div>
 
         </div>
     </div>
--- src/components/LatestCollections.js
+++ src/components/LatestCollections.js
@@ -1,9 +1,40 @@
 import React from 'react'
 import navigation from '../assets/navigation.png'
 import Collection from './Collection'
+import bag from "../assets/bag.png"
+import shoe from "../assets/pinkshoe.png"
+import gown from "../assets/150577_BROWN_9 2.png"
 
 
 export default function LatestCollections() {
+  const collections = [
+    {id: 1,
+      image: bag,
+      category: "Women",
+      rating: "2k+ rating",
+      item: 'French Kiss Bag',
+      brand: "ALDO",
+      price: "$500"
+    },
+    {id: 2,
+      image: shoe,
+      category: "Kids",
+       rating: "1k+ rating",
+       item: "Burberry shine",
+       brand: "FENDI",
+       price: "$150"
+    },
+    {id: 3,
+      image: gown,
+category: "Women",
+ rating: "5k+ rating",
+ item: "Alvero Gown",
+ brand: "DIVINE",
+ price: "$300"
+    }
+  ]
+
+  
   return (
     <div className=' pt-20 px-20'>
         <div className=' flex justify-between items-center'>
@@ -13,9 +44,9 @@ export default function LatestCollections() {
         </div>
         </div>
         <div className=' flex mt-12'>
-<Collection  />
-<Collection/>
-<Collection/>
+          {collections.map((coll)=>(
+<Collection key={coll.id} image={coll.image} category={coll.category} item={coll.item} brand={coll.brand} rating={coll.rating} price={coll.price}/>
+          ))}
 </div>
 
         </div>
